0a99199d19cc354d65de794dd99e68f5f9f419b8
nohhhe <hyunho028@gmail.com>
2025-01-12 21:24:31 +0900

    first commit

---
+++ src/main/kotlin/com/example/aiservice/AiServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.example.aiservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AiServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<AiServiceApplication>(*args)
+}
